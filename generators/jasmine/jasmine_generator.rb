600068ee9ee509bcd7e47cd8d4e4dbf2548c2ee7
Nate Clark <nate@nateclark.com>
2011-02-08 23:54:37 +0800

    load jasmine.rake using a Railtie if available (Rails >= 3.0) instead of copying it into the project

--- generators/jasmine/jasmine_generator.rb
+++ generators/jasmine/jasmine_generator.rb
@@ -1,27 +1,28 @@
 class JasmineGenerator < Rails::Generator::Base
   def manifest
-    record do |m|
+    m = Rails::Generator::Manifest.new
 
-      m.directory "public/javascripts"
-      m.file "jasmine-example/src/Player.js", "public/javascripts/Player.js"
-      m.file "jasmine-example/src/Song.js", "public/javascripts/Song.js"
+    m.directory "public/javascripts"
+    m.file "jasmine-example/src/Player.js", "public/javascripts/Player.js"
+    m.file "jasmine-example/src/Song.js", "public/javascripts/Song.js"
 
-      m.directory "spec/javascripts"
-      m.file "jasmine-example/spec/PlayerSpec.js", "spec/javascripts/PlayerSpec.js"
+    m.directory "spec/javascripts"
+    m.file "jasmine-example/spec/PlayerSpec.js", "spec/javascripts/PlayerSpec.js"
 
-      m.directory "spec/javascripts/helpers"
-      m.file "jasmine-example/spec/SpecHelper.js", "spec/javascripts/helpers/SpecHelper.js"
+    m.directory "spec/javascripts/helpers"
+    m.file "jasmine-example/spec/SpecHelper.js", "spec/javascripts/helpers/SpecHelper.js"
 
-      m.directory "spec/javascripts/support"
-      m.file "spec/javascripts/support/jasmine_runner.rb", "spec/javascripts/support/jasmine_runner.rb"
-      m.file "spec/javascripts/support/jasmine_config.rb", "spec/javascripts/support/jasmine_config.rb"
-      m.file "spec/javascripts/support/jasmine-rails.yml", "spec/javascripts/support/jasmine.yml"
+    m.directory "spec/javascripts/support"
+    m.file "spec/javascripts/support/jasmine_runner.rb", "spec/javascripts/support/jasmine_runner.rb"
+    m.file "spec/javascripts/support/jasmine_config.rb", "spec/javascripts/support/jasmine_config.rb"
+    m.file "spec/javascripts/support/jasmine-rails.yml", "spec/javascripts/support/jasmine.yml"
+    m.readme "INSTALL"
 
+    unless Gem.available?('rails', '>= 3.0')
       m.directory "lib/tasks"
       m.file "lib/tasks/jasmine.rake", "lib/tasks/jasmine.rake"
-
-      m.readme "INSTALL"
     end
+    m
   end
 
   def file_name
--- lib/jasmine.rb
+++ lib/jasmine.rb
@@ -10,3 +10,13 @@ jasmine_files << 'generator' if Gem.available? "rails", ">= 3.0"
 jasmine_files.each do |file|
   require File.join('jasmine', file)
 end
+
+if Jasmine.rails3?
+  module Jasmine
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        load "jasmine/tasks/jasmine.rake"
+      end
+    end
+  end
+end
