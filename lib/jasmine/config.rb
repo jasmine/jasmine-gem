2566c4bd4d988e6a6e363c40811dfaaf44cf9a81
Brendan Erwin <brendanjerwin@gmail.com>
2011-05-13 21:14:36 -0400

    Ignores helpers if in require.js mode.
    Modified the erb to expect that the require.js configuration be an array. Now any require.js configuration can be done prior to using require.js.

--- lib/jasmine/config.rb
+++ lib/jasmine/config.rb
@@ -93,8 +93,8 @@ module Jasmine
     def js_files(spec_filter = nil)
       spec_files_to_include = spec_filter.nil? ? spec_files : match_files(spec_dir, [spec_filter])
       files = []
-      files = src_files.collect {|f| "/" + f } unless require_js
-      files + helpers.collect {|f| File.join(spec_path, f) } + spec_files_to_include.collect {|f| File.join(spec_path, f) }
+      files = src_files.collect {|f| "/" + f } + helpers.collect {|f| File.join(spec_path, f) } unless require_js
+      files + spec_files_to_include.collect {|f| File.join(spec_path, f) }
     end
 
     def css_files
