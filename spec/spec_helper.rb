405cb59a37009d1431dddc150816418ac5a58f8d
ragaskar <ragaskar@gmail.com>
2012-04-13 19:00:42 -0700

    Add ability to run a debugger with 1.9.3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,9 @@ require "bundler"
 require 'stringio'
 require 'tmpdir'
 
-Bundler.setup(:default, :development)
+envs = [:default, :development]
+envs << :debug if ENV["DEBUG"]
+Bundler.setup(*envs)
 
 $:.unshift(File.expand_path(File.join(File.dirname(__FILE__), "../lib")))
 require "jasmine"
