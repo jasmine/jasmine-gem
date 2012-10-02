f035f22e6139f695edf8d0b3cdcb1aa1ad7c44cb
Jeff Keen <jeff@keen.me>
2012-10-01 21:16:58 -0500

    Check the running version of rails as well as the rails gem version to avoid problems when both rails 2.x and 3.x are in the gemset, fixes #108

--- lib/jasmine/dependencies.rb
+++ lib/jasmine/dependencies.rb
@@ -7,15 +7,15 @@ module Jasmine
       end
 
       def rails2?
-        safe_gem_check("rails", "~> 2.3")
+        safe_gem_check("rails", "~> 2.3") && running_rails2?
       end
 
       def legacy_rails?
-        safe_gem_check("rails", "< 2.3.11")
+        safe_gem_check("rails", "< 2.3.11") && running_legacy_rails?
       end
 
       def rails3?
-        safe_gem_check("rails", ">= 3.0")
+        safe_gem_check("rails", ">= 3.0") && running_rails3?
       end
 
       def legacy_rack?
@@ -27,6 +27,23 @@ module Jasmine
       end
 
       private
+
+      def running_legacy_rails?
+        running_rails? && (Gem::Version.new(Rails.version) < Gem::Version.new("2.3.11"))
+      end
+
+      def running_rails2?
+        running_rails? && Rails.version.to_i == 2
+      end
+
+      def running_rails3?
+        running_rails? && Rails.version.to_i == 3
+      end
+
+      def running_rails?
+        defined?(Rails) && Rails.respond_to?(:version)
+      end
+
       def safe_gem_check(gem_name, version_string)
         if Gem::Specification.respond_to?(:find_by_name)
           Gem::Specification.find_by_name(gem_name, version_string)
