85b562608a4f4dc35f881c61ae7ac660e0c55480
Dima Kovalenko <dima@groupon.com>
2012-11-21 19:59:56 -0500

    Adding 2 specs to describe the current result processor's search for example_locations

--- spec/results_processor_spec.rb
+++ spec/results_processor_spec.rb
@@ -1,3 +1,25 @@
-describe Jasmine::ResultsProcessor do
+require 'spec_helper'
 
+describe Jasmine::ResultsProcessor do
+ describe "example locations finder" do
+   describe "simple file layout" do
+     it "should find groups that start with describe" do     
+       spec_files_full_paths = ['spec/fixture/spec/example_spec.js']
+       user_config = double('config', :spec_files_full_paths => spec_files_full_paths)
+       config = Jasmine::RunnerConfig.new(user_config)
+       runner = Jasmine::ResultsProcessor.new(config)
+     
+       runner.example_locations["example_spec"].should == "spec/fixture/spec/example_spec.js:1: in `it'"
+     end
+   
+     it "should find examples that start with it" do
+       spec_files_full_paths = ['spec/fixture/spec/example_spec.js']
+       user_config = double('config', :spec_files_full_paths => spec_files_full_paths)
+       config = Jasmine::RunnerConfig.new(user_config)
+       runner = Jasmine::ResultsProcessor.new(config)
+          
+       runner.example_locations["example_spec should be here for path loading tests"].should == "spec/fixture/spec/example_spec.js:2: in `it'"
+     end
+  end
+ end
 end
