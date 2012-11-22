9d5baff9f7a9a1b64300550d5b1e6cfca846870c
Dima Kovalenko <dima@groupon.com>
2012-11-21 21:36:17 -0500

    Adding tests for nested groups and examples

--- spec/results_processor_spec.rb
+++ spec/results_processor_spec.rb
@@ -2,35 +2,35 @@ require 'spec_helper'
 
 describe Jasmine::ResultsProcessor do
  describe "example locations finder" do
-   # describe "simple file layout" do
-   #   it "should find groups that start with describe" do     
-   #     spec_files_full_paths = ['spec/fixture/spec/example_spec.js']
-   #     user_config = double('config', :spec_files_full_paths => spec_files_full_paths)
-   #     config = Jasmine::RunnerConfig.new(user_config)
-   #     runner = Jasmine::ResultsProcessor.new(config)
-   #   
-   #     runner.example_locations["example_spec"].should == "spec/fixture/spec/example_spec.js:1: in `it'"
-   #   end
-   # 
-   #   it "should find examples that start with it" do
-   #     spec_files_full_paths = ['spec/fixture/spec/example_spec.js']
-   #     user_config = double('config', :spec_files_full_paths => spec_files_full_paths)
-   #     config = Jasmine::RunnerConfig.new(user_config)
-   #     runner = Jasmine::ResultsProcessor.new(config)
-   #        
-   #     runner.example_locations["example_spec should be here for path loading tests"].should == "spec/fixture/spec/example_spec.js:2: in `it'"
-   #   end
-   # end
+   describe "simple file layout" do
+     it "should find groups that start with describe" do     
+       spec_files_full_paths = ['spec/fixture/spec/example_spec.js']
+       user_config = double('config', :spec_files_full_paths => spec_files_full_paths)
+       config = Jasmine::RunnerConfig.new(user_config)
+       runner = Jasmine::ResultsProcessor.new(config)
+     
+       runner.example_locations["example_spec"].should == "spec/fixture/spec/example_spec.js:1: in `it'"
+     end
+   
+     it "should find examples that start with it" do
+       spec_files_full_paths = ['spec/fixture/spec/example_spec.js']
+       user_config = double('config', :spec_files_full_paths => spec_files_full_paths)
+       config = Jasmine::RunnerConfig.new(user_config)
+       runner = Jasmine::ResultsProcessor.new(config)
+          
+       runner.example_locations["example_spec should be here for path loading tests"].should == "spec/fixture/spec/example_spec.js:2: in `it'"
+     end
+   end
   
    describe "nested groups" do
-     # it "should contain the name of a nested group" do
-     #   spec_files_full_paths = ['spec/fixture/spec/example_spec.js']
-     #    user_config = double('config', :spec_files_full_paths => spec_files_full_paths)
-     #    config = Jasmine::RunnerConfig.new(user_config)
-     #    runner = Jasmine::ResultsProcessor.new(config)
-     # 
-     #    runner.example_locations.keys.include?("example_spec nested_groups").should == true
-     # end
+     it "should contain the name of a nested group" do
+       spec_files_full_paths = ['spec/fixture/spec/example_spec.js']
+        user_config = double('config', :spec_files_full_paths => spec_files_full_paths)
+        config = Jasmine::RunnerConfig.new(user_config)
+        runner = Jasmine::ResultsProcessor.new(config)
+     
+        runner.example_locations.keys.include?("example_spec nested_groups").should == true
+     end
      
      it "should contain the full name of a nested example" do
        spec_files_full_paths = ['spec/fixture/spec/example_spec.js']
