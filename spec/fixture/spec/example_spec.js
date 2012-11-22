06cd6e3983fdebc5ecd833052c7a90c12bd2d2cf
Dima Kovalenko <dima@groupon.com>
2012-11-21 21:37:21 -0500

    Adding a nested group into an example

--- spec/fixture/spec/example_spec.js
+++ spec/fixture/spec/example_spec.js
@@ -2,4 +2,10 @@ describe("example_spec", function() {
   it("should be here for path loading tests", function() {
     expect(true).toBe(true);
   }
+  
+  describe("nested_groups", function() {
+    it("should contain the full name of nested example", function(){
+	    expect(true).toBe(true);
+	  })
+  })
 })
