455d1be94c6e2263990127810c61f1a3d7804dff
Gregg Van Hove and Rajan Agaskar <pair+gvanhove+rajan@pivotallabs.com>
2013-09-18 10:15:43 -0700

    Simplify Rails specs
    - Just test the things we really care about: rake -T presence, rake jasmine:ci execution

--- spec/jasmine_rails_spec.rb
+++ spec/jasmine_rails_spec.rb
@@ -20,6 +20,14 @@ if Jasmine::Dependencies.rails_available?
 
       Bundler.with_clean_env do
         puts `NOKOGIRI_USE_SYSTEM_LIBRARIES=true bundle install --path vendor;`
+        `bundle exec rails g jasmine:install`
+        File.exists?('spec/javascripts/helpers/.gitkeep').should == true
+        File.exists?('spec/javascripts/support/jasmine.yml').should == true
+        `bundle exec rails g jasmine:examples`
+        File.exists?('app/assets/javascripts/jasmine_examples/Player.js').should == true
+        File.exists?('app/assets/javascripts/jasmine_examples/Song.js').should == true
+        File.exists?('spec/javascripts/jasmine_examples/PlayerSpec.js').should == true
+        File.exists?('spec/javascripts/helpers/SpecHelper.js').should == true
       end
     end
 
@@ -27,66 +35,24 @@ if Jasmine::Dependencies.rails_available?
       temp_dir_after
     end
 
-    context 'when Jasmine has been required' do
-      it 'should show the Jasmine generators' do
-        Bundler.with_clean_env do
-          output = `bundle exec rails g`
-          output.should include('jasmine:install')
-          output.should include('jasmine:examples')
-        end
+    it 'should have the jasmine & jasmine:ci rake task' do
+      #See https://github.com/jimweirich/rake/issues/220 and https://github.com/jruby/activerecord-jdbc-adapter/pull/467
+      #There's a workaround, but requires setting env vars & jruby opts (non-trivial when inside of a jruby process), so skip for now.
+      pending "activerecord-jdbc + rake -T doesn't work correctly under Jruby" if ENV['RAILS_VERSION'] == 'rails3' && RUBY_PLATFORM == 'java'
+      Bundler.with_clean_env do
+        output = `bundle exec rake -T`
+        output.should include('jasmine ')
+        output.should include('jasmine:ci')
       end
-
-      it 'should show jasmine:install help' do
-        Bundler.with_clean_env do
-          output = `bundle exec rails g jasmine:install --help`
-          output.should include('rails generate jasmine:install')
-        end
-      end
-
-      it 'should have the jasmine & jasmine:ci rake task' do
-        #See https://github.com/jimweirich/rake/issues/220 and https://github.com/jruby/activerecord-jdbc-adapter/pull/467
-        #There's a workaround, but requires setting env vars & jruby opts (non-trivial when inside of a jruby process), so skip for now.
-        pending "activerecord-jdbc + rake -T doesn't work correctly under Jruby" if ENV['RAILS_VERSION'] == 'rails3' && RUBY_PLATFORM == 'java'
-        Bundler.with_clean_env do
-          output = `bundle exec rake -T`
-          output.should include('jasmine ')
-          output.should include('jasmine:ci')
-        end
-      end
-
-      context 'and then installed' do
-        before :each do
-          Bundler.with_clean_env do
-            @output = `bundle exec rails g jasmine:install`
-          end
-        end
-
-        it 'should have the Jasmine config files' do
-          @output.should include('create')
-
-          File.exists?('spec/javascripts/helpers/.gitkeep').should == true
-          File.exists?('spec/javascripts/support/jasmine.yml').should == true
-        end
-
-        context 'and the jasmine examples have been installed' do
-          it 'should find the Jasmine example files' do
-            Bundler.with_clean_env do
-              output = `bundle exec rails g jasmine:examples`
-              output.should include('create')
-
-              File.exists?('app/assets/javascripts/jasmine_examples/Player.js').should == true
-              File.exists?('app/assets/javascripts/jasmine_examples/Song.js').should == true
-
-              File.exists?('spec/javascripts/jasmine_examples/PlayerSpec.js').should == true
-              File.exists?('spec/javascripts/helpers/SpecHelper.js').should == true
-
-              output = `bundle exec rake jasmine:ci`
-              output.should include('5 specs, 0 failures')
-            end
-          end
-        end
-      end
-
     end
+
+    it "rake jasmine:ci runs and returns expected results" do
+      Bundler.with_clean_env do
+        output = `bundle exec rake jasmine:ci`
+        output.should include('5 specs, 0 failures')
+      end
+    end
+
+
   end
 end
