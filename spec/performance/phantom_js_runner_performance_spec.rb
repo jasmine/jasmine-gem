3bd49d30bebec7cea8ee9e9100bc596a40545bc7
Gregg Van Hove <pair+gvanhove@pivotallabs.com>
2013-09-26 10:08:08 -0700

    Fix performance specs
    - Command line no longer installs examples by default, so they don't
      need to be counted
    - No longer need rake -E bootstrap stuff, so remove it'

--- spec/performance/phantom_js_runner_performance_spec.rb
+++ spec/performance/phantom_js_runner_performance_spec.rb
@@ -15,13 +15,9 @@ describe "Phantom JS Runner performance/integration suite", :performance => true
   it 'handles deeply nested/large test suites' do
     Jasmine::CommandLineTool.new.process ['init']
 
-    my_jasmine_lib = File.expand_path(File.join(@root, 'lib'))
-    bootstrap = "$:.unshift('#{my_jasmine_lib}')"
-
     FileUtils.cp(File.join(@root, 'spec', 'fixture', 'large_test_suite_spec.js'), File.join(@tmp, 'spec', 'javascripts'))
 
-    ci_output = `rake -E "#{bootstrap}" --trace jasmine:ci`
-    ci_output.should =~ (/40005 specs, 20000 failures/)
+    ci_output = `rake --trace jasmine:ci`
+    ci_output.should =~ (/40000 specs, 20000 failures/)
   end
-
 end
