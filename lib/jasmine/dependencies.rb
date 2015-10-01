65131212838786447536146dd0ae14d2487faf2d
Elana Koren and Travis Grathwell <pair+ekoren+tgrathwell@pivotal.io>
2015-10-01 16:49:52 -0700

    Rails 5 support
    The only notable difference seems to be that
    `assets_environment.find_asset(pathname)` always returns
    a single asset instead of an array of assets.
    
    Does not yet include Rails 5 in the test matrix because it's alpha

--- lib/jasmine/dependencies.rb
+++ lib/jasmine/dependencies.rb
@@ -3,38 +3,29 @@ module Jasmine
 
     class << self
       def rails3?
-        running_rails3?
+        rails? && Rails.version.to_i == 3
       end
 
       def rails4?
-        running_rails4?
+        rails? && Rails.version.to_i == 4
+      end
+
+      def rails5?
+        rails? && Rails.version.to_i == 5
       end
 
       def rails?
-        running_rails?
+        defined?(Rails) && Rails.respond_to?(:version)
       end
+
       def legacy_rack?
         !defined?(Rack::Server)
       end
 
       def use_asset_pipeline?
-        assets_pipeline_available = (rails3? || rails4?) && Rails.respond_to?(:application) && Rails.application.respond_to?(:assets)
+        assets_pipeline_available = (rails3? || rails4? || rails5?) && Rails.respond_to?(:application) && Rails.application.respond_to?(:assets)
         rails3_assets_enabled = rails3? && assets_pipeline_available && Rails.application.config.assets.enabled != false
-        assets_pipeline_available && (rails4? || rails3_assets_enabled)
-      end
-
-      private
-
-      def running_rails3?
-        running_rails? && Rails.version.to_i == 3
-      end
-
-      def running_rails4?
-        running_rails? && Rails.version.to_i == 4
-      end
-
-      def running_rails?
-        defined?(Rails) && Rails.respond_to?(:version)
+        assets_pipeline_available && (rails4? || rails5? || rails3_assets_enabled)
       end
     end
   end
