739e0f9b93173155ed5ae26ddf6f1201cb864063
Sam Joseph <tansaku@gmail.com>
2016-08-11 10:37:08 +0900

    allows us to set a different name for the hostname used by the ci runner

--- lib/jasmine/configuration.rb
+++ lib/jasmine/configuration.rb
@@ -6,6 +6,7 @@ module Jasmine
     attr_accessor :jasmine_path, :spec_path, :boot_path, :src_path, :image_path, :runner_boot_path, :helper_files
     attr_accessor :jasmine_dir, :spec_dir, :boot_dir, :src_dir, :images_dir, :runner_boot_dir
     attr_accessor :formatters
+    attr_accessor :hostname
     attr_accessor :host
     attr_accessor :spec_format
     attr_accessor :runner
@@ -92,6 +93,10 @@ module Jasmine
 
     def host
       @host || 'http://localhost'
+    end
+
+    def hostname
+      @hostname || 'localhost'
     end
 
     private
